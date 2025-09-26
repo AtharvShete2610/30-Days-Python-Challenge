6d2bd2edde600073c7204b4fb83a01174a3932ee
AtharvShete2610 <sheteatharv1609@gmail.com>
2025-09-26 14:48:38 +0530

    Day 3

---
+++ Day 3 Assignment.py
@@ -0,0 +1,14 @@
+# You are given an integer array arr[]. The task is to find the sum of it.
+
+# Examples:
+
+# Input: arr[] = [1, 2, 3, 4]
+# Output: 10
+# Explanation: 1 + 2 + 3 + 4 = 10.
+# Input: arr[] = [1, 3, 3]
+# Output: 7
+# Explanation: 1 + 3 + 3 = 7.
+
+def sum_arr(arr):
+    return sum(arr)
+print(sum_arr([1,2,3,4,5]))
