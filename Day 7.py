eba0ecca436ce6deee2b564252ab0589b7fa1daf
AtharvShete2610 <sheteatharv1609@gmail.com>
2025-09-30 16:54:56 +0530

    Day 7

---
+++ Day 7.py
@@ -0,0 +1,26 @@
+# Prime Number
+# Difficulty: EasyAccuracy: 22.2%Submissions: 412K+Points: 2
+# Given a number n, determine whether it is a prime number or not.
+# Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+
+# Examples :
+
+# Input: n = 7
+# Output: true
+# Explanation: 7 has exactly two divisors: 1 and 7, making it a prime number.
+# Input: n = 25
+# Output: false
+# Explanation: 25 has more than two divisors: 1, 5, and 25, so it is not a prime number.
+# Input: n = 1
+# Output: false
+# Explanation: 1 has only one divisor (1 itself), which is not sufficient for it to be considered prime.
+
+import math
+def isPrime(n):
+    if n <= 1:
+        return 0
+    for i in range(2,int(math.sqrt(n))+1):
+        if n % 2 == 0:
+            return 0
+    return 1
+print(isPrime(25))
