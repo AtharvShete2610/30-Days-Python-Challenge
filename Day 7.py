94c0f48e738ced93e0af962106a76dc2de0838ad
AtharvShete2610 <sheteatharv1609@gmail.com>
2025-10-05 22:10:59 +0530

    Updation

--- Day 7.py
+++ Day 7.py
@@ -20,7 +20,7 @@ def isPrime(n):
     if n <= 1:
         return 0
     for i in range(2,int(math.sqrt(n))+1):
-        if n % 2 == 0:
+        if n % i == 0:
             return 0
     return 1
 print(isPrime(25))
