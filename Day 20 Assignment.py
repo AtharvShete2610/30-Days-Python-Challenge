0015b6001a691ccf50a6e7aa23eef1be6f5c2e6b
AtharvShete2610 <sheteatharv1609@gmail.com>
2025-10-13 16:28:24 +0530

    Day 20

---
+++ Day 20 Assignment.py
@@ -0,0 +1,18 @@
+# Lower case to upper case
+# Difficulty: BasicAccuracy: 74.85%Submissions: 36K+Points: 1
+# Given a string str containing only lowercase letters, generate a string with the same letters, but in uppercase.
+
+# Example 1:
+
+# Input:
+# str = "geeks"
+# Output: GEEKS
+# Example 2:
+
+# Input:
+# str = "geeksforgeeks"
+# Output: GEEKSFORGEEKS
+
+def toUpper(s):
+    return s.upper()
+print(toUpper("geeks"))
